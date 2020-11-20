4340653da93c133813a33e1fe243de25e4625072
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2020-11-19 23:26:11 -0300

    se agrega los modelos al sitio de administracion.

--- SGC/bodega/admin.py
+++ SGC/bodega/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Batch,Insumo,Producto,Tipo_cerveza
 # Register your models here.
+
+admin.site.register(Batch)
+admin.site.register(Insumo)
+admin.site.register(Producto)
+admin.site.register(Tipo_cerveza)
+
+
+admin.site.site_header = "Sistema Gestion Cervecero"
+admin.site.site_title = "Portal Admin SGC"
+admin.site.index_title = "Bienvenidos al portal de administración"
