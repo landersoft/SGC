7dea2575c5c802779ab91573714f7846ccfc2ae3
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2020-11-24 18:52:10 -0300

    se registran modelos nuevos

--- SGC/ventas/admin.py
+++ SGC/ventas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente,Detalle_Venta, Venta
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Detalle_Venta)
+admin.site.register(Venta)
--- SGC/ventas/models.py
+++ SGC/ventas/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from bodega.models import Producto, Tipo_cerveza
+from django.conf import settings 
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.TextField(max_length=100)
+    telefono = models.CharField(max_length=12)
+    def __str__(self):
+        return self.nombre
+
+
+class Venta(models.Model):
+
+    ENT_PAG = "Entregado y pagado"
+    ENT_NOPAG = "Entregado y no pagado"
+    NOENT_PAG = "No entregado y pagado"
+    NOENT_NOPAG = "No entregado y no pagado"
+
+    ESTADO = [
+       (ENT_PAG, "Entregado y pagado"),
+       (ENT_NOPAG, "Entregado y no pagado"),
+       (NOENT_PAG, "No entregado y pagado"),
+       (NOENT_NOPAG, "No entregado y no pagado"), 
+    ]
+    cliente = models.ForeignKey('Cliente', related_name="cliente", on_delete=models.PROTECT, verbose_name="cliente")
+    usuario =  models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True, null=True)
+    estado = models.CharField(max_length=50, choices=ESTADO)
+
+class Detalle_Venta(models.Model):
+    venta = models.ForeignKey('Venta', related_name="Venta", on_delete=models.PROTECT, verbose_name="venta")
+    producto = models.ForeignKey('Producto', related_name="Producto", on_delete=models.PROTECT, verbose_name="producto")
+    cantidad = models.IntegerField()
+    def __str__(self):
+        return str(self.venta)
