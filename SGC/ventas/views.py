405117ffc6f2ee9cf7a13fdddf16b80c997eda42
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2020-11-24 22:05:30 -0300

    cambio en la ruta del template

--- SGC/ventas/views.py
+++ SGC/ventas/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'templates/ventas/index.html')
+    return render(request, '/ventas/index.html')
